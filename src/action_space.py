c299e9b89da3da0704e9d71c17185a2557d1f60d
James Gilmore <142340349+ProvenNoIQ@users.noreply.github.com>
2024-05-02 13:23:51 -0700

    Added Flexibility
    Added more requirement functionality for types of degrees and field of degrees. Also updated the check job/edu functions to get a possible action space function. Will need work on the state to make fully function (specifically have education history be a list of their educations to account for having different types of degrees (masters of math + masters of econ)

--- src/action_space.py
+++ src/action_space.py
@@ -1,9 +1,10 @@
 import random
 class Job:
-    def __init__(self,salary, education_requirements, work_requirements,low_raise,high_raise,years_in) -> None:
+    def __init__(self,salary, edu_level_req, edu_type_req, work_experience_req,low_raise,high_raise,years_in) -> None:
         self.salary=salary
-        self.education_requirements=education_requirements
-        self.work_experience=work_requirements
+        self.edu_level_req=edu_level_req
+        self.education_type_req=edu_type_req
+        self.work_experience_req=work_experience_req
         self.years_since_raise=0
         self.lower_raise_bound=low_raise
         self.upper_raise_bound=high_raise
@@ -15,32 +16,52 @@ class Job:
                 self.salary=self.salary+ self.years_since_raise*random.uniform(self.lower_raise_bound,
                                                                                self.upper_raise_bound)
     def __str__(self):
-        return f"Job(salary={self.salary}, education_requirements='{self.education_requirements}', work_experience={self.work_experience}, raise_range=({self.lower_raise_bound}-{self.upper_raise_bound}))"
+        return f"Job(salary={self.salary}, edu_level_req={self.edu_level_req}, education_type_req={self.education_type_req}, work_experience_req={self.work_experience_req}, raise_range=({self.lower_raise_bound}-{self.upper_raise_bound})), years_in={self.years_in}"
                 
 class Education:
-    def __init__(self,general_ed,years_in,cost) -> None:
-        self.general_ed=general_ed
+    def __init__(self, edu_degree, edu_field, years_in, years_for_degree, cost, edu_level_req, edu_type_req) -> None:        
+        self.edu_degree=edu_degree
+        self.edu_field=edu_field
         self.years_in=years_in
+        self.years_for_degree=years_for_degree
         self.cost=cost
-    def __str__(self)
-        return f"Job(general_ed={self.general_ed}, years_in='{self.years_in}', cost={self.cost}"
+        self.edu_level_req=edu_level_req
+        self.edu_type_req=edu_type_req
+    def __str__(self):
+        return f"Job(edu_field={self.edu_field}, edu_degree='{self.edu_degree}', years_in={self.years_in}, years_for_degree={self.years_for_degree}, cost={self.cost}"
+    
 #cost=0 for general eds years_in depends on user input        
-general_ed=Education(True,0,0)
-comp_sci=Education(False,0,100)
-economics=Education(False,0,123)
-literature=Education(False,0,1203)
-math=Education(False,0,241)
-edu_board=[general_ed,comp_sci,economics,literature,math]
+general_ed=Education("High_School","General_Ed", 10,12,0,None,None)
+bachelors_math=Education("Bacherlors","Math", 0,4,10000,"High_School","General_Ed")
+masters_math=Education("Masters","Math", 0,2,10000,"Bachelors","Math")
+education_board=[general_ed,bachelors_math,masters_math]
 #Job board would be the same sort of thing just defining everything
 
 
 def check_job_requirements(state, job):
     #check if state stuff passes requirements
-    return True
+    if state.years_of_experience<job.work_experience_required:
+        return False
+    field=job.edu_type_req
+    degree=job.edu_level_req
+    if field and degree is None:
+        return True
+    for education in state.education:
+        if education.edu_degree==degree and education.edu_field==field:
+            return True
+    return False
+    
 
 def check_edu_requirements(state, edu):
     #check if state stuff passes requirements
-    return True
+    if edu.edu_requirement is None:
+        return True
+    degree=edu.edu_level_req
+    field=edu.edu_type_req
+    for education in state.education:
+        if education.edu_degree==degree and education.edu_field==field:
+            return True
+    return False
 
 def job_options(state, jobs):
     possible_jobs=[]
@@ -49,13 +70,14 @@ def job_options(state, jobs):
             possible_jobs.append(job)
     return possible_jobs
 
-def education_options(state, education):
+def education_options(state, educations):
     possible_edu=[]
-    for edu in education:
-        if check_edu_requirements(state,education): 
+    for edu in educations:
+        if check_edu_requirements(state,edu): 
             possible_edu.append(edu)
     return possible_edu
 
 def actions(state, jobs, education):
-    actions=job_options(state,jobs)+education_options(state,education)
-    return actions
+    action_set=[]
+    action_set=job_options(state,jobs)+education_options(state,education)
+    return action_set
